830ae44708a0431414326488b364d9d53bced223
Onoriode <onoriodeumukoro8@gmail.com>
2025-06-09 03:38:29 +0100

    code adjusted

--- models/message.js
+++ models/message.js
@@ -10,7 +10,15 @@ const messageSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "Users", required: true
     },
-    conversation: []
+    conversation: [
+        {
+            id: { type: String, required: true, trim: true },
+            message: { type: String, required: true, trim: true }, 
+            senderId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Users" },
+            createdAt: { type: Date, required: true, trim: true },
+            time: { type: String, required: true, trim: true }
+        }
+    ]
 })
 
 const messageModel = mongoose.model("Conversations", messageSchema)
